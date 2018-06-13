d480cf2a95c45c3ea5daf74ca7013651e7130a00
ckboyjiy <ckboyjiy@nate.com>
2018-06-13 23:57:39 +0900

    TSP 일단 구현

---
+++ advanced/graph/travelling_salesman_problem/TSP.spec.js
@@ -0,0 +1,31 @@
+import { expect } from 'chai';
+import { TSP } from "./TSP";
+
+describe('TSP Algorithm test', () => {
+    it('case 1', () => {
+        const graph = new TSP();
+
+        graph.addVertex('A');
+        graph.addVertex('B');
+        graph.addVertex('C');
+        graph.addVertex('D');
+        graph.addVertex('S');
+
+        graph.addEdge('A', 'B', 2);
+        graph.addEdge('A', 'C', 5);
+        graph.addEdge('A', 'D', 1);
+
+        graph.addEdge('B', 'C', 3);
+        graph.addEdge('B', 'D', 6);
+
+        graph.addEdge('C', 'D', 4);
+
+        graph.addEdge('S', 'D', 8);
+        graph.addEdge('S', 'C', 7);
+
+        const result = graph.shortest();
+        expect(result).to.equal(21);
+
+        console.log('aaa');
+    });
+})
